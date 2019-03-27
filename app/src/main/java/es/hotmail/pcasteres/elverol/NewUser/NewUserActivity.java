c330853b73e03b2417a0a884f9e32f8a145ab80c
Cristian <criscastemendo@gmail.com>
2019-03-27 13:48:32 +0000

    Añadir Color de fondo texto

--- app/src/main/java/es/hotmail/pcasteres/elverol/NewUser/NewUserActivity.java
+++ app/src/main/java/es/hotmail/pcasteres/elverol/NewUser/NewUserActivity.java
@@ -23,7 +23,7 @@ public class NewUserActivity
         EditText Nombre = (EditText) findViewById(R.id.nombre);
         String nombre = Nombre.getText().toString();
         EditText User = (EditText) findViewById(R.id.usuario);
-        String user = Nombre.getText().toString();
+        String user = User.getText().toString();
 
 
 
